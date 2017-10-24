c736c0b82232bbab2693b6d28b8f53f72604b7c8
Jeroen <dijkvan@gmail.com>
2017-10-24 14:14:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ function getAdress(){
 	var zipcode = $("#zipcode").val();
 	var housenr = $("#housenr").val();
 
-	if(zipcode != "" && housenr != ""){
+	if(zipcode && housenr){
 		$.ajax({
 		  url: "https://postcode-api.apiwise.nl/v2/addresses/",
-		  headers: {'X-API-Key': 'rqNVKLf9Ks4OMr2xL4TtI1sYDvAUkpjB1TjRIeDW'},
+		  headers: {'X-API-Key': 'your-key'},
 		  data: {
 		    postcode: zipcode,
 		    number: housenr
 		  },
 		  success: function( result ) {
-		  	console.log(result);
 		    $( "#street" ).text(result._embedded.addresses[0].street);
 		    $( "#city" ).text(result._embedded.addresses[0].city.label);
 		  }
